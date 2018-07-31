5ed46f352f21f160c8f5d86a7ac2dd45b5680871
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2018-08-01 08:21:37 +0900

    Morphemeクラスを削除

--- src/main/scala/midomoji/Main.scala
+++ src/main/scala/midomoji/Main.scala
@@ -10,47 +10,29 @@ object Main {
   
   def main(args: Array[String]): Unit = {
     args.toList match {
-      // -b ./dictionary/matrix.def ./dictionary/morpheme.csv ./dictionary/dictionary_set.bin
-      case ("-b" | "--build") :: mtPath :: ptPath :: dictPath :: xs => {
-        val matrix = Util.createMT(mtPath);
-        val prefixtree = Util.createPT(ptPath);
-        DictionarySet[Morpheme](prefixtree, matrix).serialize(dictPath);
-      }
-
-      // -cm ./dictionary/dictionary_set.bin ./dictionary/matrix.def
-      case ("-cm" | "--check-matrix") :: dictPath :: mtPath :: xs => {
-        val dictSet = DictionarySet[Morpheme](dictPath);
-        val mt = dictSet.matrix;
-        Util.checkMT(mt, mtPath);
-      }
-
-      // -cp ./dictionary/dictionary_set.bin ./dictionary/morpheme.csv
-      case ("-cp" | "--check-prefixtree") :: dictPath :: ptPath :: xs => {
-        val dictSet = DictionarySet[Morpheme](dictPath);
-        val pt = dictSet.prefixtree;
-        Util.checkPT(pt, ptPath);
-      }
-
-      // --build-simple ./dictionary/matrix.def ./dictionary/morpheme.csv ./dictionary/dictionary_set_simple.bin
-      case ("--build-simple") :: mtPath :: ptPath :: dictPath :: xs => {
+      // --build ./dictionary/matrix.def ./dictionary/morpheme.csv ./dictionary/dictionary_set.bin
+      case ("--build") :: mtPath :: ptPath :: dictPath :: xs => {
         val matrix = Util.createMT(mtPath);
         val prefixtree = Util.createPTSimple(ptPath);
         DictionarySet[Array[Int]](prefixtree, matrix).serialize(dictPath);
       }
 
-      // --check-prefixtree-simple ./dictionary/dictionary_set_simple.bin ./dictionary/morpheme.csv
-      case ("--check-prefixtree-simple") :: dictPath :: ptPath :: xs => {
+      // --check-matrix ./dictionary/dictionary_set.bin ./dictionary/matrix.def
+      case ("-cm" | "--check-matrix") :: dictPath :: mtPath :: xs => {
+        val dictSet = DictionarySet[Array[Int]](dictPath);
+        val mt = dictSet.matrix;
+        Util.checkMT(mt, mtPath);
+      }
+
+      // --check-prefixtree ./dictionary/dictionary_set.bin ./dictionary/morpheme.csv
+      case ("--check-prefixtree") :: dictPath :: ptPath :: xs => {
         val dictSet = DictionarySet[Array[Int]](dictPath);
         val pt = dictSet.prefixtree;
         Util.checkPTSimple(pt, ptPath);
       }
 
       case ("-d" | "--debug") :: mode :: xs => {
-        if (mode == "simple") {
-          debug2();
-        } else {
-          debug();
-        }
+        debug();
       }
       case ("-a" | "--analysis") :: xs  => {
         println("解析");
@@ -66,89 +48,6 @@ object Main {
   }
 
   def debug(): Unit = {
-    var prefixtree: PrefixTree[Morpheme] = PrefixTree[Morpheme](500000);
-    var matrix: Matrix = Matrix(1316, 1316);
-    def go(): Unit = {
-      print("command : ");
-      readLine.split(" ").toList match {
-        case "exit" :: xs => return ();
-        case "deserialize" :: dict :: xs => {
-          val dictSet = DictionarySet[Morpheme](dict);
-          prefixtree = dictSet.prefixtree;
-          matrix = dictSet.matrix;
-        }
-        case "create" :: dict :: xs => {
-          prefixtree = Util.createPT(dict);
-        }
-        case "check" :: ptPath :: xs => {
-          Util.checkPT(prefixtree, ptPath);
-        }
-        case "cost" :: l :: r :: xs => {
-          val left  = Util.toIntOption(l);
-          val right = Util.toIntOption(r);
-          if (matrix != null && left != None && right != None) {
-            println(matrix.getCost(left.get, right.get));
-          } else {
-            println("matrix is null or invalid args");
-          }
-        }
-        case "find" :: surface :: xs => {
-          prefixtree.find(surface) match {
-            case None    => println("not found");
-            case Some(m) => println(m);
-          }
-        }
-        case "search" :: surface :: xs => {
-          val len = surface.length;
-          (0 until len).foreach { i =>
-            val sub = surface.slice(i, len);
-            val ms = prefixtree.prefixSearch(sub).flatMap(e => e._2).mkString(", ");
-            println("%d : %s".format(i + 1, ms));
-          }
-        }
-        case "add" :: surface :: xs => {
-          val m = Morpheme(surface, 1, 1, 0, "", "", "");
-          prefixtree.add(surface, m);
-        }
-        case "init" :: xs => {
-          prefixtree = PrefixTree[Morpheme](500000);
-          matrix = Matrix(1316, 1316);
-        }
-        case "dump" :: xs => {
-          prefixtree.dump;
-        }
-        case "status" :: xs => {
-          if (prefixtree != null && matrix != null) {
-            println("----- matrix -----");
-            println("leftSize=%d, rightSize=%d".format(matrix.leftSize, matrix.rightSize));
-            println("----- prefixtree -----");
-            println("size=%d".format(prefixtree.size));
-          } else {
-            println("dict is null");
-          }
-        }
-        case _ => {
-          val help = ListBuffer[String]();
-          help += "exit                : デバッグモードを終了する";
-          help += "deserialize [DICT]  : 構築済み辞書を読み込む";
-          help += "create [DICT]       : ";
-          help += "check [DICT] [PT]   : ";
-          help += "cost [LEFT] [RIGHT] : ";
-          help += "find [SURFACE]      : ";
-          help += "search [TEXT]       : ";
-          help += "add [SURFACE]       : ";
-          help += "init                : ";
-          help += "dump                : ";
-          help += "status              : ";
-          println(help.mkString("\n"));
-        }
-      }
-      go();
-    }
-    go();
-  }
-
-  def debug2(): Unit = {
     var prefixtree: PrefixTree[Array[Int]] = PrefixTree[Array[Int]](500000);
     var matrix: Matrix = Matrix(1316, 1316);
     def go(): Unit = {
--- src/main/scala/midomoji/Util.scala
+++ src/main/scala/midomoji/Util.scala
@@ -80,66 +80,6 @@ object Util {
     }
     // 正しく登録されているかチェック
     if (Using[Source, Boolean](Source.fromFile(mtPath))(checkMatrix)) println("OK!!") else println("NG...");
-
-  }
-
-  def parsePT(input: String): Option[Morpheme] = {
-    val list    = input.split(",");
-    if (list.length == 13) {
-      val surface = Normalizer.normalize(list(0), Normalizer.Form.NFKC);
-      val leftId  = Util.toIntOption(list(1));
-      val rightId = Util.toIntOption(list(2));
-      val cost    = Util.toIntOption(list(3));
-      if (leftId != None && rightId != None && cost != None) {
-        // 表層形,左文脈ID,右文脈ID,コスト,品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用型,活用形,原形,読み,発音
-        val morpheme = Morpheme(surface, leftId.get, rightId.get, cost.get, list(4), list(10), list(11));
-        return Some(morpheme);
-      }
-    }
-    return None;
-  }
-
-  def createPT(path: String): PrefixTree[Morpheme] = {
-    Using[Source, PrefixTree[Morpheme]](Source.fromFile(path)) { s =>
-      val pt = PrefixTree[Morpheme](500000);
-      s.getLines.foreach { line =>
-        Util.parsePT(line) match {
-          case None           => ();
-          case Some(morpheme) => pt.add(morpheme.surface, morpheme);
-        }
-      }
-      pt;
-    }
-  }
-
-  def checkPT(prefixree: PrefixTree[Morpheme], ptPath: String): Unit = {
-    val checkPT: Source => Boolean = s => {
-      // 表層形,左文脈ID,右文脈ID,コスト,品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用型,活用形,原形,読み,発音
-      var cnt = 0;
-      var ret = s.getLines.foldLeft(true) { (ret, line) =>
-        Util.parsePT(line) match {
-          case None           => ret;
-          case Some(morpheme) => {
-            val morphemes = prefixree.find(morpheme.surface);
-            val exists = morphemes match {
-              case None     => false;
-              case Some(ms) => ms.exists(m => if (morpheme == m) true else false);
-            }
-            if (exists) {
-              ret;
-            } else {
-              println("input=%s, output=%s".format(morpheme.surface, morphemes));
-              cnt += 1;
-              false;
-            }
-          }
-        }
-      }
-      println("failcnt = %d".format(cnt));
-      ret;
-    }
-    // 正しく登録されているかチェック
-    if (Using[Source, Boolean](Source.fromFile(ptPath))(checkPT)) println("OK!!") else println("NG...");
   }
 
   def parsePTSimple(input: String): Option[(String, Array[Int])] = {
